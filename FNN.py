beaf5fbb3a3080a694d4e29dfb1e70bf3840db27
kostadinova-gergana <156928717+kostadinova-gergana@users.noreply.github.com>
2024-05-27 16:29:22 +0300

    Updated FNN for 5 classes.py

--- FNN.py
+++ FNN.py
@@ -11,46 +11,38 @@ import matplotlib.pyplot as plt
 from sklearn.utils import resample
 from sklearn.metrics import precision_score, recall_score, f1_score
 
-# Plotting function for accuracy-related metrics
 def plot_accuracy_metrics(history):
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['acc'], label='Training Accuracy')
+    plt.plot(history.history['val_acc'], label='Validation Accuracy')
+    plt.title('Model Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
 
-    # Accuracy
-    plt.plot(history.history['accuracy'], label='Accuracy')
-    plt.plot(history.history['val_accuracy'], label='Val_Accuracy')
-
-    # Precision
-    plt.plot(history.history['precision'], label='Precision')
-    plt.plot(history.history['val_precision'], label='Val_Precision')
-
-    # Recall
-    plt.plot(history.history['recall'], label='Recall')
-    plt.plot(history.history['val_recall'], label='Val_Recall')
-
-    # F1 score
-    plt.plot(history.history['f1_score'], label='F1 Score')
-    plt.plot(history.history['val_f1_score'], label='Val_F1 Score')
-
-    plt.title('Accuracy Metrics')
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['precision'], label='Training Precision')
+    plt.plot(history.history['val_precision'], label='Validation Precision')
+    plt.plot(history.history['recall'], label='Training Recall')
+    plt.plot(history.history['val_recall'], label='Validation Recall')
+    plt.plot(history.history['f1_score'], label='Training F1 Score')
+    plt.plot(history.history['val_f1_score'], label='Validation F1 Score')
+    plt.title('Precision, Recall, and F1 Score')
     plt.xlabel('Epoch')
     plt.ylabel('Score')
     plt.legend()
-    plt.grid(True)
     plt.show()
 
-# Plotting function for loss-related metrics
 def plot_loss_metrics(history):
-    plt.figure(figsize=(12, 6))
-
-    # Loss
-    plt.plot(history.history['loss'], label='Loss')
-    plt.plot(history.history['val_loss'], label='Val_Loss')
-
-    plt.title('Loss Metrics')
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Model Loss')
     plt.xlabel('Epoch')
-    plt.ylabel('Value')
+    plt.ylabel('Loss')
     plt.legend()
-    plt.grid(True)
     plt.show()
 
 # Load data and print first few lines of the dataset
@@ -64,22 +56,25 @@ data['age'] = np.where((data.age > 100), np.nan, data.age)
 # Removing redundant attributes from thyroidDF dataset
 data.drop(['TSH_measured', 'T3_measured', 'TT4_measured', 'T4U_measured', 'FTI_measured', 'TBG_measured', 'patient_id', 'referral_source', 'query_on_thyroxine', 'query_hypothyroid', 'query_hyperthyroid', 'hypopituitary'], axis=1, inplace=True)
 
-# Create a list with the allowed values
-allowed_values = ['-','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+# Create a list with allowed data
+allowed_values = ['-','F', 'I', 'G', 'K']
 
-# Filter df for the allowed values
+# Filter df by allowed data
 filtered_data = data[data['target'].isin(allowed_values)]
 
+# Print unique values in the 'target' column
+unique_values = filtered_data['target'].unique()
+
+# Print the length of the DataFrame
+data_length_after_drop = len(filtered_data)
+
 # Re-mapping target values to diagnostic groups
-diagnoses = {'-': '0', # healthy
-             'A': '1', # hyperthyroid
-             'B': '1', 
-             'C': '1', 
-             'D': '1',
-             'E': '2', # hypothyroid
-             'F': '2', 
-             'G': '2', 
-             'H': '2'}
+diagnoses = {'-': '1', # healthy
+             'F': '2', # primary hypothyroid
+             'I': '3', # increased binding protein 
+             'G': '4', # compensated hypothyroid
+             'K': '5' # concurrent non-thyroidal illness
+            }
 
 filtered_data.loc[:, 'target'] = filtered_data['target'].map(diagnoses)
 filtered_data['target'] = pd.to_numeric(filtered_data['target'], errors='coerce')
@@ -105,6 +100,7 @@ filtered_data['sex'] = np.where((filtered_data.sex.isnull()) & (filtered_data.pr
 
 # Count missing values per row
 filtered_data['n_missing'] = filtered_data.isnull().sum(axis=1)
+sns.histplot(filtered_data, x='n_missing', binwidth=0.5, hue='target')
 
 # Remove rows with 3 or more missing values
 filtered_data.drop(filtered_data.index[filtered_data['n_missing'] > 2], inplace=True)
@@ -120,47 +116,46 @@ filtered_data.drop(['thyroid_surgery', 'pregnant', 'sick', 'lithium', 'on_antith
 
 # OVER- AND UNDERSAMPLING TO BALANCE DATA
 
+# Set the random seed for reproducibility
+np.random.seed(42)
+
 # Resample 500 samples from the healthy individuals class
-healthy_samples = resample(filtered_data[filtered_data['target'] == 0], 
+healthy_samples = resample(filtered_data[filtered_data['target'] == 1], 
                            replace=False,  # Sampling without replacement
-                           n_samples=500)  # Number of samples to select
+                           n_samples=400)  # Number of samples to select
 
 # Concatenate the resampled healthy samples with the rest of the data
-filtered_data_resampled = pd.concat([filtered_data[filtered_data['target'] != 0], healthy_samples])
+filtered_data_resampled = pd.concat([filtered_data[filtered_data['target'] != 1], healthy_samples])
 
 # Shuffle the dataset to randomize the order
 filtered_data_resampled = filtered_data_resampled.sample(frac=1).reset_index(drop=True)
 
-# Upsample the second class to increase its count to 400
-hyperthyroidism_samples_upsampled = resample(filtered_data_resampled[filtered_data_resampled['target'] == 1], 
-                                            replace=True,  # Sampling with replacement
-                                            n_samples=400)  # Number of samples to select
-
-# Concatenate the upsampled hyperthyroidism samples with the rest of the data
-filtered_data_resampled_upsampled = pd.concat([filtered_data_resampled[filtered_data_resampled['target'] != 1], hyperthyroidism_samples_upsampled])
-
-# Shuffle the dataset to randomize the order
-filtered_data_resampled_upsampled = filtered_data_resampled_upsampled.sample(frac=1).reset_index(drop=True)
+# Check the class balance after resampling
+print(filtered_data_resampled['target'].value_counts())
 
 # PRINCIPAL COMPONENT ANALYSIS
 
-# Define X and y
-X = filtered_data.drop(columns=['target'])
-y = filtered_data['target']
+# Define X and y using the resampled data
+X_resampled = filtered_data_resampled.drop(columns=['target'])
+y_resampled = filtered_data_resampled['target']
 
-# Standardise the features
+# Standardize the features
 scaler = StandardScaler()
-X_standardized = scaler.fit_transform(X)
+X_resampled_standardized = scaler.fit_transform(X_resampled)
 
 # Fit PCA and transform the standardized features
-pca = PCA()
-X_pca = pca.fit_transform(X_standardized)
+pca_resampled = PCA()
+X_resampled_pca = pca_resampled.fit_transform(X_resampled_standardized)
+
+# Fit PCA with 8 components on the resampled data
+pca_resampled = PCA(n_components=7)
+X_resampled_pca = pca_resampled.fit_transform(X_resampled_standardized)
 
 # FNN MODEL
 
 # Define the input features (X) and target variable (y)
-X = filtered_data_resampled_upsampled.drop(columns=['target'])
-y = filtered_data_resampled_upsampled['target']
+X = X_resampled_pca
+y = y_resampled - 1  # Adjust labels to be in range 0-4
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -171,7 +166,7 @@ model = Sequential([
     Dropout(0.2),
     Dense(32, activation='relu'),
     Dropout(0.5),
-    Dense(3, activation='softmax')
+    Dense(5, activation='softmax')  # Updated to 5 classes
 ])
 
 # Compile the model
@@ -197,35 +192,36 @@ val_f1_score = f1_score(y_test, np.argmax(val_predictions, axis=1), average='mac
 history.history['precision'] = train_precision
 history.history['recall'] = train_recall
 history.history['f1_score'] = train_f1_score
+
 history.history['val_precision'] = val_precision
 history.history['val_recall'] = val_recall
 history.history['val_f1_score'] = val_f1_score
 
-# Evaluate the model on the test set
-test_loss, test_accuracy = model.evaluate(X_test, y_test, verbose=0)
-print('Test Accuracy:', test_accuracy)
-
-# Plot accuracy-related metrics
-plot_accuracy_metrics(history)
-
-# Plot loss-related metrics
-plot_loss_metrics(history)
-
 # After training, to print accuracy and loss metrics
 train_loss, train_accuracy = model.evaluate(X_train, y_train, verbose=0)
 val_loss, val_accuracy = model.evaluate(X_test, y_test, verbose=0)
 
+# Calculate precision, recall, and F1-score during training
+train_predictions = model.predict(X_train)
+val_predictions = model.predict(X_test)
+
+train_precision = precision_score(y_train, np.argmax(train_predictions, axis=1), average='macro')
+train_recall = recall_score(y_train, np.argmax(train_predictions, axis=1), average='macro')
+train_f1_score = f1_score(y_train, np.argmax(train_predictions, axis=1), average='macro')
+
+val_precision = precision_score(y_test, np.argmax(val_predictions, axis=1), average='macro')
+val_recall = recall_score(y_test, np.argmax(val_predictions, axis=1), average='macro')
+val_f1_score = f1_score(y_test, np.argmax(val_predictions, axis=1), average='macro')
+
+# Print accuracy and loss metrics
 print("Training Accuracy:", train_accuracy)
 print("Training Loss:", train_loss)
+print("Training Precision:", train_precision)
+print("Training Recall:", train_recall)
+print("Training F1 Score:", train_f1_score)
+
 print("Validation Accuracy:", val_accuracy)
 print("Validation Loss:", val_loss)
-
-
-# After training, to print accuracy and loss metrics
-train_loss, train_accuracy = model.evaluate(X_train, y_train, verbose=0)
-val_loss, val_accuracy = model.evaluate(X_test, y_test, verbose=0)
-
-print("Training Accuracy:", train_accuracy)
-print("Training Loss:", train_loss)
-print("Validation Accuracy:", val_accuracy)
-print("Validation Loss:", val_loss)
+print("Validation Precision:", val_precision)
+print("Validation Recall:", val_recall)
+print("Validation F1 Score:", val_f1_score)
